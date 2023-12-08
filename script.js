d33131de6b202eddfa571ea156d63fb39ea42333
rkcenci <rkcenci@gmail.com>
2023-12-07 21:30:26 -0300

    ajuste rotacao

--- script.js
+++ script.js
@@ -1,30 +1,41 @@
-const multipleItemCarousel = document.querySelector("#carousel_sec_espiritual");
 
-var carousel = new bootstrap.Carousel(multipleItemCarousel, {
-  keyboard: false
+const refreshCarousel = () => {
+
+  const multipleItemCarousel = document.querySelector("#carousel_sec_espiritual");
+
+  new bootstrap.Carousel(multipleItemCarousel, {
+    keyboard: false
+  });
+
+  if (window.matchMedia("(min-width:728px)").matches) {
+    $(multipleItemCarousel).removeClass("slide");
+    $("#carousel_sec_espiritual .carousel-inner .carousel-item.active").removeClass("active");
+
+    var cardWidth = $("#carousel_sec_espiritual .carousel-item").width();
+    var enabledControl = 'right';
+
+    $("#carousel_sec_espiritual .carousel-control-next").on("click", function () {
+      if (enabledControl === 'right') {
+        $("#carousel_sec_espiritual .carousel-inner .carousel-item").animate({ left: -3 * cardWidth + 'px' }, 1500 );
+        enabledControl = 'left';
+      }
+    });
+
+    $("#carousel_sec_espiritual .carousel-control-prev").on("click", function () {
+      if (enabledControl === 'left') {
+        $("#carousel_sec_espiritual .carousel-inner .carousel-item").animate({ left: '0px' }, 1500 ); 
+        enabledControl = 'right';  
+      }
+    });
+
+  } else {
+    $("#carousel_sec_espiritual .carousel-inner .carousel-item").first().addClass("active");
+    $(multipleItemCarousel).addClass("slide");
+  }
+}
+
+window.addEventListener("orientationchange", function() {
+  refreshCarousel();
 });
 
-
-if (window.matchMedia("(min-width:728px)").matches) {
-
-  var cardWidth = $("#carousel_sec_espiritual .carousel-item").width();
-  var enabledControl = 'right';
-
-  $("#carousel_sec_espiritual .carousel-control-next").on("click", function () {
-    if (enabledControl === 'right') {
-      $("#carousel_sec_espiritual .carousel-inner .carousel-item").animate({ left: -3 * cardWidth + 'px' }, 1500 );
-      enabledControl = 'left';
-    }
-  });
-
-  $("#carousel_sec_espiritual .carousel-control-prev").on("click", function () {
-    if (enabledControl === 'left') {
-       $("#carousel_sec_espiritual .carousel-inner .carousel-item").animate({ left: '0px' }, 1500 ); 
-       enabledControl = 'right';  
-    }
-  });
-
-} else {
-  $("#carousel_sec_espiritual .carousel-inner .carousel-item").first().addClass("active");
-  $(multipleItemCarousel).addClass("slide");
-}
+refreshCarousel();
