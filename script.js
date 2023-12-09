64b46f5b9788e16d66f3cb5bddb3e7f1f8d52a31
rkcenci <rkcenci@gmail.com>
2023-12-09 15:18:48 -0300

    ajustes

--- script.js
+++ script.js
@@ -16,14 +16,14 @@ const refreshCarousel = () => {
 
     $("#carousel_sec_espiritual .carousel-control-next").on("click", function () {
       if (enabledControl === 'right') {
-        $("#carousel_sec_espiritual .carousel-inner .carousel-item").animate({ left: -3 * cardWidth + 'px' }, 1500 );
+        $("#carousel_sec_espiritual .carousel-inner .carousel-item").animate({ left: -3 * cardWidth + 'px' }, 1000 );
         enabledControl = 'left';
       }
     });
 
     $("#carousel_sec_espiritual .carousel-control-prev").on("click", function () {
       if (enabledControl === 'left') {
-        $("#carousel_sec_espiritual .carousel-inner .carousel-item").animate({ left: '0px' }, 1500 ); 
+        $("#carousel_sec_espiritual .carousel-inner .carousel-item").animate({ left: '0px' }, 1000 ); 
         enabledControl = 'right';  
       }
     });
@@ -34,43 +34,46 @@ const refreshCarousel = () => {
   }
 }
 
+const menuItems = () => {
+  $("#sobre").click(function() {
+    $('html,body').animate({
+        behavior: 'smooth',
+        scrollTop: $("#sec_ambiente").offset().top},
+        'slow');
+  });
+
+  $("#formacao").click(function() {
+    $('html,body').animate({
+        behavior: 'smooth',
+        scrollTop: $("#sec_formacao").offset().top},
+        'slow');
+  });
+
+  $("#atividades").click(function() {
+    $('html,body').animate({
+        behavior: 'smooth',
+        scrollTop: $("#sec_atividades").offset().top},
+        'slow');
+  });
+
+  $("#contribua").click(function() {
+    $('html,body').animate({
+        behavior: 'smooth',
+        scrollTop: $("#sec_contribua").offset().top},
+        'slow');
+  });
+
+  $("#contato").click(function() {
+    $('html,body').animate({
+        behavior: 'smooth',
+        scrollTop: $("#sec_contato").offset().top},
+        'slow');
+  });
+}
+
 window.addEventListener("orientationchange", function() {
   location.reload();
 });
 
 refreshCarousel();
-
-$("#sobre").click(function() {
-  $('html,body').animate({
-      behavior: 'smooth',
-      scrollTop: $("#sec_ambiente").offset().top},
-      'slow');
-});
-
-$("#formacao").click(function() {
-  $('html,body').animate({
-      behavior: 'smooth',
-      scrollTop: $("#sec_formacao").offset().top},
-      'slow');
-});
-
-$("#atividades").click(function() {
-  $('html,body').animate({
-      behavior: 'smooth',
-      scrollTop: $("#sec_atividades").offset().top},
-      'slow');
-});
-
-$("#contribua").click(function() {
-  $('html,body').animate({
-      behavior: 'smooth',
-      scrollTop: $("#sec_contribua").offset().top},
-      'slow');
-});
-
-$("#contato").click(function() {
-  $('html,body').animate({
-      behavior: 'smooth',
-      scrollTop: $("#sec_contato").offset().top},
-      'slow');
-});
+menuItems();
